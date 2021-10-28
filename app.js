85762b27249947464c6daa9c04e0ccec72d22ee5
Salvador Villalon <salvadorvillalon54@gmail.com>
2021-10-27 17:26:50 -0700

    add

--- app.js
+++ app.js
@@ -36,6 +36,8 @@ app.use(favicon(path.join(__dirname, 'public', 'images', 'sal.png')));
 
 // Set up mongoose connection
 const mongoDB = process.env.DEV_MONGODB_URI || process.env.MONGODB_URI;
+console.log('What is mongoDB');
+console.log({ mongoDB });
 // const mongoDB = process.env.DEV_MONGODB_URI;
 mongoose.connect(mongoDB, { useNewUrlParser: true, useUnifiedTopology: true });
 const db = mongoose.connection;
